41a65885c5324a2164ff80d3795aaf33519681bc
sumit9722 <shahsumit9722@gmail.com>
2024-10-22 00:07:46 +0530

    preloader added complete flip added description commented

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import InfoBox from './InfoBox';
 import CardBack from './CardBack';
 import {gsap} from 'gsap';
-import {useGSAP} from '@gsap/react';
+
+import dvm from "/assets/prateek.png";
+import adp from "/assets/shreyansh.png";
+import pcr from "/assets/aryan.png";
+import controls from "/assets/jaiditya.png";
+import recnacc from "/assets/rijul.png";
+import spons from "/assets/shreeram.png";
+import gensec from "/assets/aryankhorana.png";
+import prez from "/assets/ahan.png"
+
+
 
 export default function App() {
 
@@ -12,53 +22,52 @@ export default function App() {
     {
       name : "Prateek Kashyap",
       depName : "Department of Visual Media",
-      imgLink : "/assets/prateek.png"
+      imgLink : dvm
     },
     {
       name : "Shreyansh Vanjani",
       depName : "Department of Art, Design and Publicity",
-      imgLink : "assets/shreyansh.png"
+      imgLink : adp
     },
     {
       name : "Aryan Wadlwa",
       depName : "Department of Publications and Correspondence",
-      imgLink : "assets/aryan.png"
+      imgLink : pcr
 
     },
     {
       name : "Jaiditya Singh",
       depName : "Department of Controls",
-      imgLink : "assets/jaiditya.png"
+      imgLink : controls
 
     },
     {
       name : "Rijul Bassamboo",
       depName : "Department of Reception and Accommodation",
-      imgLink: "assets/rijul.png"
+      imgLink: recnacc
 
     },
     {
       name : "Shreeram Verma",
       depName : "Department of Sponsorship and Marketing",
-      imgLink : "assets/shreeram.png"
-
+      imgLink : spons
     },
     {
       name : "Aryan Khorana",
       depName : "General Secretary, Students' Union",
-      imgLink : "assets/aryankhorana.png"
-
+      imgLink : gensec
     },
     {
       name : "Ahan Bansal",
       depName : "President, Students' Union",
-      imgLink : "assets/ahan.png"
+      imgLink : prez
 
     }
   ];
 
   const [currIndex, setCurrIndex] = useState(1);
   const [checker, setChecker] = useState(true);
+  const [degcheck, setDegcheck] = useState(true);
   const currIndexRef = useRef(currIndex);
   const checkerRef = useRef(checker);
   const currCard = useRef();
@@ -89,20 +98,36 @@ export default function App() {
 
   
   useEffect(() => {
+    const preloadArray = [dvm, adp, pcr, controls, recnacc, spons, gensec, prez]
+
+    for (let asset of preloadArray) {
+      let img = new Image();
+      img.src = asset;
+    }
+
     gsap.defaults({
       ease: "power2.inOut",
       duration:1,
     });
     
     var tl = gsap.timeline({defaults: {ease: "power2.inOut"}});
-    tl.to(currCard.current, {
-      rotateY: 0,
-      duration: 0.5
-    });
+    if(degcheck)
+    {
+      tl.to(currCard.current, {
+        rotateY:0,
+      });
+    }
+    else{
+      tl.to(currCard.current, {
+        rotateY:360,
+      });
+    }
+    
     tl.to(currCard.current, {
       width: '80vw',
       delay: 0.3
     },0)
+    setDegcheck(!degcheck);
   }, [currIndex]);
 
   useEffect(() => {
